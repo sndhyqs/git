a3c4636226b774a1ba2be7e5e661ce952353d003
yanqingshan <529357231@qq.com>
2014-11-20 14:59:55 +0800

    增加: *mymain类 里面包含基本相对独立的方法 修改: *验证码的识别方法 *index文件的修改

--- image.class.php
+++ image.class.php
@@ -4,71 +4,74 @@ require_once 'imageTool.class.php';
 
 class YImage {
 
-    private $MAX = 1000;
-    private $MIN = 6000000;
     private $Iresource;
     private $white;
     private $hight;
-    private $DataArray;
+    private $dataArray;
+    private $dataLenth;
 
     public function __construct($Iresource) {
         $this->Iresource = $Iresource;
         $this->white = imagesx($this->Iresource);
         $this->hight = imagesy($this->Iresource);
+        $this->dataLenth = $this->white * $this->hight;
     }
 
-    public function compare($othres) {
-        for ($x = 0; $x < $this->white; $x++) {
-            for ($y = 0; $y < $this->hight; $y++) {
-                $myindex = ImageColorAt($this->Iresource, $x, $y);
-                $otherindex = ImageColorAt($othres, $x, $y);
-                if ($myindex != $otherindex) {
-                    return FALSE;
-                }
-            }
-        }
-        return TRUE;
-    }
-
-    public function compare1($dataArray) {
-
+    /**
+     * 根据验证码的相同度判断是否为同一个
+     * @param type $dataArray
+     * @return boolean
+     */
+    public function compare($dataArray) {
         $tag = 0;
-        for ($x = 0; $x < 140; $x++) {
-            if ($this->DataArray[$x] == $dataArray[$x]) {
+        for ($x = 0; $x <  $this->dataLenth; $x++) {
+            if ($this->dataArray[$x] == $dataArray[$x]) {
                 $tag++;
-                if ($tag <= 135)
-                    return FALSE;
+                if ($tag >=  $this->dataLenth-5)
+                    return TRUE;
             }
         }
-        return TRUE;
+        return FALSE;
     }
 
+    /**
+     * 根据rgb去噪
+     * 原理是因为验证码和背景色的rgb不同
+     * @return image
+     */
     public function quzhao() {
         $data = array();
         for ($x = 0; $x < $this->white; $x++) {
             for ($y = 0; $y < $this->hight; $y++) {
-
                 $index = ImageColorAt($this->Iresource, $x, $y);
                 $rgbarray = imagecolorsforindex($this->Iresource, $index);
                 if ($rgbarray['red'] < 78 || $rgbarray['green'] < 110 || $rgbarray['blue'] < 110) {
                     array_push($data, 1);
+                    //   echo "*";
                 } else {
                     array_push($data, 0);
-                    //imagefill($this->Iresource, $x, $y, self::getWhitePaint());
+                    imagefill($this->Iresource, $x, $y, self::getWhitePaint());
+                    //  echo "-";
                 }
+                //  echo " ";
             }
+            // echo " <br/>";
         }
-
-        $this->DataArray = $data;
+        // echo "<br/>";
+        $this->dataArray = $data;
         return $this->Iresource;
     }
 
+    /**
+     * 白色画笔
+     * @return type
+     */
     public function getWhitePaint() {
         return imagecolorallocate($this->Iresource, 0xFF, 0xFF, 0xFF);
     }
 
     public function getDataAray() {
-        return $this->DataArray;
+        return $this->dataArray;
     }
 
     public function getImage() {
--- index.php
+++ index.php
@@ -1,50 +1,16 @@
 <?php
-
-ini_set('error_reporting', 'E_ALL ^ E_NOTICE');
+//ini_set('error_reporting', 'E_ALL ^ E_NOTICE');
 require_once 'file.class.php';
 require_once 'image.class.php';
-$filearray = File::getFileName("./image/data");
-//$checkArray = array();
-//foreach ($filearray as $value) {
-//    $image = new YImage(imagecreatefromjpeg("./image/data/$value.jpg"));
-//    $image->quzhao();
-//    echo $str = implode('', $image->getDataAray());
-//    echo $str. "---" . $value;
-//    if ($checkArray[$str]) {
-//        echo "-------------- " . $checkArray[$str];
-//    }
-//    $checkArray[$str] = $value;
-//
-//    echo "<br/>";
-//}
-$length = count($filearray);
-for ($x = 0; $x < $length; $x++) {
-    $image = new YImage(imagecreatefromjpeg("./image/data/$filearray[$x].jpg"));
-    $image->quzhao();
-    for ($y = $x + 1; $y < $length; $y++) {
-        $image1 = new YImage(imagecreatefromjpeg("./image/data/$filearray[$y].jpg"));
-        $image1->quzhao();
-        if ($image->compare1($image1->getDataAray())) {
-            echo "success$filearray[$x]=>$filearray[$y]";
-        }
-    }
-    echo "<br/>";
+require_once 'mymain.class.php';
+require_once 'checkcode.class.php';
+$checkCode = new CheckCode();
+$code = $checkCode->discernCheckCode();
+if (strlen($code) == 4) {
+    echo "识别成功";
+} else {
+    echo "识别失败";
 }
-
-
-//foreach ($filearray as $value) {
-//
-//    $im = imagecreatefromjpeg("./image/$value.jpg");
-//    //"http://210.42.38.26:81/jwc_glxt/ValidateCode.aspx");
-//
-//    $newimage = imagecreate(10, 14);
-//    //imagecopy($newimage, $im, 0, 0, 5, 5, 10, 14);
-//    //imagecopy($newimage, $im, 0, 0, 17, 5, 10, 14);
-//    //imagecopy($newimage, $im, 0, 0, 28, 5, 10, 14);
-//    imagecopy($newimage, $im, 0, 0, 40, 5, 10, 14);
-//    $image = new YImage($newimage);
-//    $image->quzhao();
-//    $value = $value + 66;
-//    YImageTool::save($newimage, "./image/data/$value.jpg");
-//}
+echo $code;
 ?>
+<img src="./image/temp.jpg"/>
